1d48f00b3822955ec87bc67547f3b7832a74eeb8
Patricio Tisalema <pstisalema@outlook.com>
2026-05-27 11:30:09 -0500

    fix: add missing @Service import to ReporteService

--- src/main/java/com/empresa/sistema_ventas/service/ReporteService.java
+++ src/main/java/com/empresa/sistema_ventas/service/ReporteService.java
@@ -14,6 +14,7 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.ByteArrayInputStream;
